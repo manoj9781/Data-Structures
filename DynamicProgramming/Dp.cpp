f8f10e551d5b32694f1873697591c6fd03f076d5
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-09 01:06:20 +0530

    Fibonacci number using Memoization Technique

--- DynamicProgramming/Dp.cpp
+++ DynamicProgramming/Dp.cpp
@@ -12,8 +12,39 @@ int fibo(int n){
     return a + b;
 }
 
+/*
+Memoization technique of fibonacci Number to improve the time 
+complexity. The complexity of this function is order of n.
+Memoization technique uses bottom-down approach
+*/
+
+int fibonacciNumber(int *ans, int n){
+    if(n <= 1){
+        ans[n] = n;
+        return 1;
+    }
+    if(ans[n-1] == 0){
+        ans[n - 1] = fibonacciNumber(ans, n - 1);
+    }
+    if(ans[n-2] == 0){
+        ans[n - 2] = fibonacciNumber(ans, n - 2);
+    }
+
+    ans[n] = ans[n - 1] + ans[n - 2];
+    return ans[n];
+}
+
+int fibonacciNumber(int n){
+    int *ans = new int[n + 1];
+    for (int i = 0; i <= n; i++){
+        ans[i] = 0;
+    }
+    return fibonacciNumber(ans, n);
+}
+
 int main(){
     int n;
     cin >> n;
+    cout << fibonacciNumber(n) << endl;
     cout << fibo(n) << endl;
 }
