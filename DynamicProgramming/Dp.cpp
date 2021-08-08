ef95b80e267d40e1744d0438badb1a1de3feb13a
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-09 00:53:55 +0530

    BruteForce Approach of Fibonacci Number

--- DynamicProgramming/Dp.cpp
+++ DynamicProgramming/Dp.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+
+// Complexity of this function is 2 power of n
+// Recursive solution Brute Force approach
+int fibo(int n){
+    if(n <=1){
+        return 1;
+    }
+    int a = fibo(n - 1);
+    int b = fibo(n - 2);
+    return a + b;
+}
+
 int main(){
-    
+    int n;
+    cin >> n;
+    cout << fibo(n) << endl;
 }
