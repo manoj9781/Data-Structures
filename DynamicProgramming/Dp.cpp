5e0ac61b732864744544408d4a436d2403b26816
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-09 22:22:47 +0530

    Minimum cost Path using BruteForce recursive solution

--- DynamicProgramming/Dp.cpp
+++ DynamicProgramming/Dp.cpp
@@ -135,15 +135,43 @@ int minStepsDP(int n){
     return ans[n];
 }
 
-int main(){
-    int n;
-    cin >> n;
+int minCostPath(int ** input, int m ,int n, int i, int j){
+    if(i == m-1 && j == n-1){
+        return input[i][j];
+    }
+    if(i >= m || j >= n){
+        return INT_MAX;
+    }
 
+    int x = minCostPath(input, m, n, i, j + 1);
+    int y = minCostPath(input, m, n, i + 1, j + 1);
+    int z = minCostPath(input, m, n, i + 1, j);
+
+    int ans = min(x, min(y, z)) + input[i][j];
+    return ans;
+}
+
+int minCostPath(int **input, int m, int n){
+    return minCostPath(input, m, n, 0, 0);
+}
+
+int main(){
+    int m, n;
+    cin >> m >> n;
+    int **input = new int *[m];
+    for (int i = 0; i < m; i++){
+        input[i] = new int[n];
+        for (int j = 0; j < n; j++){
+            cin >> input[i][j];
+        }
+    }
+    cout << minCostPath(input, m, n) << endl;
+
+    /*
     cout << minStepsDP(n) << endl;
     cout << minStepsBetter(n) << endl;
-
     cout << minSteps(n) << endl;
-    /*
+
     cout << fiboBetter(n) << endl;
     cout << fibonacciNumber(n) << endl;
     cout << fibo(n) << endl;
