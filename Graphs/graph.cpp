2dad2d0a7a66fcdd04bfede91db8f75524e46f26
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-18 10:20:18 +0530

    Implemented DFS and BFS for Disconnected Graphs

--- Graphs/graph.cpp
+++ Graphs/graph.cpp
@@ -2,7 +2,7 @@
 #include<queue>
 using namespace std;
 
-void print(int **edges, int n, int startVertex, bool *visited){
+void printDFS(int **edges, int n, int startVertex, bool *visited){
     cout << startVertex << endl;
     visited[startVertex] = true;
 
@@ -14,17 +14,17 @@ void print(int **edges, int n, int startVertex, bool *visited){
             if(visited[i]){
                 continue;
             }
-            print(edges, n, i, visited);
+            printDFS(edges, n, i, visited);
         }
     }
 }
 
- void printBFS(int **edges, int n, int startingVertex){
+ void printBFS(int **edges, int n, int startingVertex, bool *visited){
      queue<int> pendingVertex;
-     bool *visited = new bool[n];
-     for (int i = 0; i < n; i++){
-         visited[i] = false;
-     }
+    //  bool *visited = new bool[n];
+    //  for (int i = 0; i < n; i++){
+    //      visited[i] = false;
+    //  }
      pendingVertex.push(startingVertex);
      visited[startingVertex] = true;
      while(pendingVertex.size() != 0){
@@ -41,7 +41,35 @@ void print(int **edges, int n, int startVertex, bool *visited){
              }
          }
      }
-     delete[] visited;
+    //  delete[] visited;
+}
+
+void BFS(int **edges, int n){
+    bool *visited = new bool[n];
+    for (int i = 0; i < n; i++){
+        visited[i] = false;
+    }
+
+    for (int i = 0; i < n; i++){
+        if(!visited[i]){
+            printBFS(edges, n, i, visited);
+        }
+    }
+    delete[] visited;
+}
+
+void DFS(int **edges, int n){
+   bool *visited = new bool[n];
+    for (int i = 0; i < n; i++){
+        visited[i] = false;
+    }
+
+    for (int i = 0; i < n; i++){
+        if(!visited[i]){
+            printDFS(edges, n, i, visited);
+        }
+    }
+    delete[] visited;
 }
 
 int main(){
@@ -68,10 +96,16 @@ int main(){
         visited[i] = false;
     }
 
+    // cout << "DFS" << endl;
+    // printDFS(edges, n, 0, visited);
+    // cout << "BFS" << endl;
+    // printBFS(edges, n, 0);
+
     cout << "DFS" << endl;
-    print(edges, n, 0, visited);
+    DFS(edges, n);
     cout << "BFS" << endl;
-    printBFS(edges, n, 0);
+    BFS(edges, n);
+
     delete[] visited;
     for (int i = 0; i < n; i++){
         delete[] edges[i];
