58987292c3b51b802d781dce59046db93c444cbe
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-24 18:54:11 +0530

    Dijkshtras Algorightm

--- Graphs/dijksatras.cpp
+++ Graphs/dijksatras.cpp
@@ -1,64 +1,50 @@
-/*
-
-Topic - Prism Algorithm
-Author - Manoj Singh Rana
-email - manojsinghrana467@gmail.com
-TIME - 24/08/2021
-
-*/
-
 #include<iostream>
 #include<climits>
-
 using namespace std;
 
-int findMinVertex(int *weight, bool* visited, int n){
+int findMinVertex(int *distance, bool *visited, int n){
     int minVertex = -1;
     for (int i = 0; i < n; i++){
-        if(!visited[i] && (minVertex == -1|| weight[i] < weight[minVertex])){
+        if(!visited[i] && (minVertex == -1 || distance[i] < distance[minVertex])){
             minVertex = i;
         }
     }
     return minVertex;
 }
 
-void prism(int **edges, int n){
-    int *parent = new int[n];
-    int *weight = new int[n];
+void dijkshtras(int **edges, int n){
+    int *distance = new int[n];
     bool *visited = new bool[n];
 
     for (int i = 0; i < n; i++){
-        weight[i] = INT_MAX;
+        distance[i] = INT_MAX;
         visited[i] = false;
     }
-    parent[0] = -1;
-    weight[0] = 0;
 
-    for (int i = 0; i < n; i++){
-        int minVertex = findMinVertex(weight, visited, n);
+    distance[0] = 0;
+
+    for(int i = 0; i< n; i++){
+        int minVertex = findMinVertex(distance, visited, n);
         visited[minVertex] = true;
+
         for (int j = 0; j < n; j++){
             if(edges[minVertex][j] != 0 && !visited[j]){
-                if(edges[minVertex][j] < weight[j]){
-                    weight[j] = edges[minVertex][j];
-                    parent[j] = minVertex;
+                int dist = distance[minVertex] + edges[j][minVertex];
+                if(dist < distance[j]){
+                    distance[j] = dist;
                 }
             }
         }
     }
-
-    for (int i = 1; i < n; i++){
-        if(parent[i] < i){
-            cout << parent[i] << " " << i << " " << weight[i] << endl;
-        }
-        else{
-            cout << i << " " << parent[i] << " " << weight[i] << endl;
-        }
+    for (int i = 0; i < n; i++){
+        cout << i << " " << distance[i] << endl;
     }
+
+    delete[] visited;
+    delete[] distance;
 }
 
 int main(){
-    cout << "Enter Number of vertices and Number of Edges" << endl;
     int n, e;
     cin >> n >> e;
     int **edges = new int *[n];
@@ -77,7 +63,7 @@ int main(){
     }
 
     cout << endl;
-    prism(edges, n);
+    dijkshtras(edges, n);
 
     for (int i = 0; i < n; i++){
         delete[] edges[i];
