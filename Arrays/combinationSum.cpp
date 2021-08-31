dcb1d66b60169762e76ceff56a4ae6e3b500c7a4
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-31 17:11:06 +0530

    Three Sum Problem

--- Arrays/combinationSum.cpp
+++ Arrays/combinationSum.cpp
@@ -36,3 +36,42 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        if(n < 3){
+           return result;
+        }
+        
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i< n-2;i++){
+            if(i == 0 || nums[i] != nums[i-1] ){
+                int j=i+1;
+                int k = n-1;
+                while(j < k){
+                    int sum = nums[i] +nums[j] +nums[k];
+                    if(sum == 0){
+                        result.push_back({nums[i], nums[j], nums[k]});
+                        while(j < k && nums[j] == nums[j+1])
+                            j++;
+                        while(j < k && nums[k] == nums[k-1])
+                            k--;
+                        j++;
+                        k--;
+                    }
+                    else if( sum > 0){
+                        k--;
+                    }
+                    else{
+                        j++;
+                    }
+                }
+            }
+        }
+        return result;
+        
+    }
+};
