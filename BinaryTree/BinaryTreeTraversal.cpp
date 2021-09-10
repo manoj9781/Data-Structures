ca383107196ae5b3650e11848f8e5e8bdb867ece
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-10 08:41:08 +0530

    Lowest common accensector of binary search tree

--- BinaryTree/BinaryTreeTraversal.cpp
+++ BinaryTree/BinaryTreeTraversal.cpp
@@ -9,38 +9,44 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root == NULL){
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        if (root == NULL)
+        {
             return {};
         }
         vector<vector<int>> ans;
         queue<TreeNode *> pendingNodes;
         pendingNodes.push(root);
-        ans.push_back({root -> val});
-        while(pendingNodes.size() != 0){
+        ans.push_back({root->val});
+        while (pendingNodes.size() != 0)
+        {
             int size = pendingNodes.size();
-             vector<int> v;
-            for(int i = 0; i <size; i++){
+            vector<int> v;
+            for (int i = 0; i < size; i++)
+            {
                 TreeNode *front = pendingNodes.front();
-            pendingNodes.pop(); 
-           
-            if(front -> left != NULL){
-                pendingNodes.push(front -> left);
-                 v.push_back(front->left -> val);
+                pendingNodes.pop();
+
+                if (front->left != NULL)
+                {
+                    pendingNodes.push(front->left);
+                    v.push_back(front->left->val);
+                }
+                if (front->right != NULL)
+                {
+                    pendingNodes.push(front->right);
+                    v.push_back(front->right->val);
+                }
             }
-            if(front -> right != NULL){
-                pendingNodes.push(front -> right);
-                 v.push_back(front->right -> val);
+
+            if (v.size() > 0)
+            {
+                ans.push_back(v);
             }
-           
-            }
-            
-            if(v.size() > 0){
-                 ans.push_back(v); 
-            }
-           
         }
         return ans;
     }
