275faf99f19f3125625ccf80b066db9b4c247649
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-11 10:09:29 +0530

    Get target copy using queue Level order Treversal

--- BinaryTree/getTargetCopy.cpp
+++ BinaryTree/getTargetCopy.cpp
@@ -32,3 +32,44 @@ public:
         return getTargetCopy(cloned, target);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* root, TreeNode* cloned, TreeNode* target) {
+        if(root == NULL){
+            return NULL;
+        }
+        
+        queue<TreeNode *>pendingNodes, clonedNodes;
+        clonedNodes.push(cloned);
+        pendingNodes.push(root);
+        while(pendingNodes.size() != 0){
+           TreeNode *front = pendingNodes.front();
+            TreeNode *clonedFront = clonedNodes.front();
+            clonedNodes.pop();
+            pendingNodes.pop();
+            if(target -> val == clonedFront -> val){
+                return clonedFront;
+            }
+            if(front -> left != NULL){
+                pendingNodes.push(front -> left);
+                clonedNodes.push(clonedFront -> left);
+            }
+            if(front -> right != NULL){
+                pendingNodes.push(front -> right);
+                clonedNodes.push(clonedFront -> right);
+            }
+        }
+        return NULL;
+    }
+};
