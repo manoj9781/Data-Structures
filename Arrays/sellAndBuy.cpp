ab7f15f43ec35a1fe588dbe509bd3ab0e585371a
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-14 13:22:43 +0530

    Completed the buy and Sell stocks

--- Arrays/sellAndBuy.cpp
+++ Arrays/sellAndBuy.cpp
@@ -20,3 +20,21 @@ public:
         return maxProfit;
     }
 };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minimum = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] < minimum){
+                minimum = prices[i];
+            }
+            if(prices[i] - minimum > maxProfit){
+                maxProfit = prices[i] - minimum;
+            }
+        }
+        return maxProfit;
+    }
+};
