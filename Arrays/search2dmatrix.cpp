dbc47f772c61166b5d34637f0b2d173d1cc52c3c
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-17 10:19:03 +0530

    search 2d Matrix Done

--- Arrays/search2dmatrix.cpp
+++ Arrays/search2dmatrix.cpp
@@ -1,27 +1,51 @@
-// Write an efficient algorithm that searches for a target value in an m x n integer matrix. The matrix has the following properties:
+Write an efficient algorithm that searches for a target value in an m x n integer matrix. The matrix has the following properties:
 
-// Integers in each row are sorted in ascending from left to right.
-// Integers in each column are sorted in ascending from top to bottom.
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
  
 
-// Example 1:
+Example 1:
 
 
-// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
-// Output: true
-// Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
 
 
-// Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
-// Output: false
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
  
 
-// Constraints:
+Constraints:
 
-// m == matrix.length
-// n == matrix[i].length
-// 1 <= n, m <= 300
-// -109 <= matrix[i][j] <= 109
-// All the integers in each row are sorted in ascending order.
-// All the integers in each column are sorted in ascending order.
-// -109 <= target <= 109
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matrix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+
+
+//Code
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+         int n = matrix.size(); 
+         int m = matrix[0].size();  
+        
+         int i=0,j=m-1;  
+        while(i<n && j>=0)
+        {
+            if(matrix[i][j]==target)  
+                return true;
+            else if(matrix[i][j]>target) 
+                j--;
+            else
+                i++;  
+        } 
+        return 0;
+    }
+};
