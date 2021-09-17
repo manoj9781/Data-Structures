cd9e6bef8546f16bb167e8f7deadd51d835aef47
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-17 19:57:46 +0530

    Combinations of the array done

--- Arrays/combinations.cpp
+++ Arrays/combinations.cpp
@@ -36,3 +36,26 @@ Constraints:
 
 1 <= n <= 20
 1 <= k <= n
+
+class Solution {
+public:
+    
+    void combine(int start, int k, vector<vector<int>> &ans, vector<int> &temp, int n){
+        if(temp.size() == k){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i = start; i <= n; i++){
+            temp.push_back(i);
+            combine(i+1, k, ans, temp, n);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+      vector<vector<int>> ans;
+        vector<int> temp;
+        combine(1, k, ans, temp, n);
+        return ans;
+       
+    }
+};
