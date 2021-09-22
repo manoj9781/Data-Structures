e3982ee0f6dc6b857964444ee7dcacb58af47f4f
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-22 16:29:45 +0530

    Even odd list done

--- Hashmap/evenOddList.cpp
+++ Hashmap/evenOddList.cpp
@@ -35,3 +35,35 @@ Constraints:
 n == number of nodes in the linked list
 0 <= n <= 104
 -106 <= Node.val <= 106
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head -> next == NULL){
+            return head;
+        }
+        
+        ListNode *odd = head;
+        ListNode *even = head -> next;
+        ListNode *evenHead = even;
+        while(even != NULL && even -> next != NULL){
+            odd -> next = even -> next;
+            odd = odd -> next;
+            even -> next = odd -> next;
+            even = even -> next;
+        }
+        odd -> next = evenHead;
+        return head;
+    }
+};
