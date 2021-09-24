99088df979a17de484d6a452d47d8248e96c40c5
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-24 12:26:22 +0530

    Palindrome list done

--- LinkedLIst/isPalindrome.cpp
+++ LinkedLIst/isPalindrome.cpp
@@ -28,3 +28,39 @@ Constraints:
 
 The number of nodes in the list is in the range [1, 105].
 0 <= Node.val <= 9
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head -> next == NULL){
+            return true;
+        }
+        ListNode *temp = head;
+        stack<int> pendingNodes;
+        while(temp != NULL){
+            pendingNodes.push(temp -> val);
+            temp = temp -> next;
+        }
+        
+        temp = head;
+        while(temp != NULL){
+            int top = pendingNodes.top();
+            pendingNodes.pop();
+            if(top != temp -> val){
+                return false;
+            }
+            temp = temp -> next;
+        }
+        return true;
+    }
+};
