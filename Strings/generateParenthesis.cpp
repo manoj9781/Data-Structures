5b67d9348c5c4949305a05854f8588c49fa83fab
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-27 22:24:10 +0530

    Generate Parenthesis done

--- Strings/generateParenthesis.cpp
+++ Strings/generateParenthesis.cpp
@@ -25,3 +25,32 @@ Output: ["()"]
 Constraints:
 
 1 <= n <= 8
+
+
+class Solution {
+public:
+    
+    void generateParenthesis(string &current, vector<string> &ans, int right, int left){
+        if(left == 0 && right == 0){
+            ans.push_back(current);
+            return;
+        }
+        if(right > 0){
+            current.push_back('(');
+            generateParenthesis(current, ans, right - 1, left);
+            current.pop_back();
+        }
+        if(right < left && left > 0){
+            current.push_back(')');
+            generateParenthesis(current, ans, right, left -1);
+            current.pop_back();
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        string current;
+        vector<string> ans;
+        generateParenthesis(current, ans, n, n);
+        return ans;
+    }
+};
