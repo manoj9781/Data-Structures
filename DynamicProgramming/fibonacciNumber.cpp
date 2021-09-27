7861c6c0fb39de2cc011e5c1856b0444b62d4005
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-27 23:40:19 +0530

    Fibonacci number done uisng DP

--- DynamicProgramming/fibonacciNumber.cpp
+++ DynamicProgramming/fibonacciNumber.cpp
@@ -36,3 +36,16 @@ Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 Constraints:
 
 0 <= n <= 30
+
+class Solution {
+public:
+    int fib(int n) {
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        ans[1] = 1;
+        for(int i = 2; i <= n; i++){
+            ans[i] = ans[i-2] + ans[i-1];
+        }
+        return ans[n];
+    }
+};
