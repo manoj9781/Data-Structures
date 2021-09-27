be0f0b430de81ebffc36eeb6786c2e8be0b53ca3
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-27 23:41:51 +0530

    Change base case

--- DynamicProgramming/fibonacciNumber.cpp
+++ DynamicProgramming/fibonacciNumber.cpp
@@ -36,12 +36,14 @@ Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 Constraints:
 
 0 <= n <= 30
-
 class Solution {
 public:
     int fib(int n) {
         vector<int> ans(n+1);
         ans[0] = 0;
+        if(n == 0){
+            return ans[0];
+        }
         ans[1] = 1;
         for(int i = 2; i <= n; i++){
             ans[i] = ans[i-2] + ans[i-1];
