7b88131b73e080d3631a9c2a5e4ae6d6a47ee292
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-28 11:11:04 +0530

    N-th fibonacci number done

--- DynamicProgramming/nthFibonacciNumber.cpp
+++ DynamicProgramming/nthFibonacciNumber.cpp
@@ -33,3 +33,24 @@ Constraints:
 
 0 <= n <= 37
 The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n ==0 ){
+            return 0;
+        }
+        else if(n == 1){
+            return 1;
+        }
+        int ans[n+1];
+        ans[0] = 0;
+        ans[1] = 1;
+        ans[2] = 1;
+        for(int i = 3; i <= n; i++){
+            ans[i] = ans[i-1]+ ans[i-2] + ans[i-3];
+        }
+        return ans[n];
+    }
+};
