4ded0fe8c71d5d0ea180501a4db12c65a750e378
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-28 11:23:17 +0530

    Climb stairs done using Dynamic Programming

--- DynamicProgramming/climbStairs.cpp
+++ DynamicProgramming/climbStairs.cpp
@@ -46,3 +46,19 @@ Constraints:
             return climbStairs(n-2) + climbStairs(n-1);
         }
   }
+
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int ans[n+1];
+        ans[0] = 1;
+        ans[1] = 1;
+        // ans[2] = 2;
+        for(int i = 2; i <= n; i++){
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n];
+    }
+};
