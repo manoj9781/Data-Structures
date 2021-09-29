9b76f5d4462615d66622d5216d83e3523a993f99
Manoj Singh <manojsinghrana467@gmail.com>
2021-09-29 07:50:40 +0530

    Min Cost climb stairs done

--- DynamicProgramming/minCostclimb.cpp
+++ DynamicProgramming/minCostclimb.cpp
@@ -32,3 +32,17 @@ Constraints:
 
 2 <= cost.length <= 1000
 0 <= cost[i] <= 999
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int ans[n+1];
+        ans[0] = 0;
+        ans[1] = 0;
+        for(int i = 2; i <= n; i++){
+            ans[i] = min(ans[i-1] + cost[i-1], ans[i-2] + cost[i-2]);
+        }
+        return ans[n];
+    }
+};
