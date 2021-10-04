c0e882c76d9f5e0613036018edf244c2d0dd52aa
Manoj Singh <manojsinghrana467@gmail.com>
2021-10-04 12:45:24 +0530

    Count sorted vowel done

--- DynamicProgramming/countSortedVowel.cpp
+++ DynamicProgramming/countSortedVowel.cpp
@@ -35,3 +35,17 @@ Output: 66045
 Constraints:
 
 1 <= n <= 50 
+
+class Solution {
+public:
+     int countVowelStrings(int n) {
+        vector<int> v(n+1);   
+         v[0] = 1;
+        for(int j = 0; j < 5; ++j){
+            for(int i = 1; i <= n; ++i){
+                v[i] += v[i-1];
+            }
+        }
+        return v[n];
+    }
+};
