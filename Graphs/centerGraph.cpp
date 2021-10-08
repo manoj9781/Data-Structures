95ea4c3f34d1bdc5e2290e3fc7fe0ff43ff71517
Manoj Singh <manojsinghrana467@gmail.com>
2021-10-08 22:29:41 +0530

    Updated for some testCases

--- Graphs/centerGraph.cpp
+++ Graphs/centerGraph.cpp
@@ -41,7 +41,7 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         int num1 = edges[0][0];
         int num2 = edges[0][1];
-        if(num1 == edges[1][0] || num2 == edges[1][1]){
+        if(num1 == edges[1][0] || num1 == edges[1][1]){
             return num1;
         }
         return num2;
