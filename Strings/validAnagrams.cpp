1545f0304d86f230b651d1a246792f9ab64c6440
Manoj Singh <manojsinghrana467@gmail.com>
2021-10-17 23:12:44 +0530

    Valid anagrams Done

--- Strings/validAnagrams.cpp
+++ Strings/validAnagrams.cpp
@@ -26,3 +26,15 @@ Constraints:
 
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters.
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t){
+            return true;
+        }
+        return false;
+    }
+};
