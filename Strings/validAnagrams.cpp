d2302657bc385eeb7243bdc8de9d87d73a9d0a71
Manoj Singh <manojsinghrana467@gmail.com>
2021-10-17 23:12:12 +0530

    Valid Anagram

---
+++ Strings/validAnagrams.cpp
@@ -0,0 +1,28 @@
+242. Valid Anagram
+Easy
+
+3415
+
+185
+
+Add to List
+
+Share
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
