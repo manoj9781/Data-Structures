b9cffb6c2e86354bc2213eeee3b2d09765c9b6eb
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-06 17:10:35 +0530

    Last index done

--- Recursion/firstIndex.cpp
+++ Recursion/firstIndex.cpp
@@ -17,6 +17,24 @@ int firstIndex(int input[], int size, int k){
         return ans+ 1;
     }
 }
+
+int lastIndex(int input[], int size, int x){
+    if(size == 0){
+        return -1;
+    }
+
+    int ans = lastIndex(input + 1, size - 1, x);
+    if(ans != -1){
+        return ans + 1;
+    }
+    if(input[0] == x){
+        return 0;
+    }
+    else{
+        return -1;
+    }
+}
+
 int main(){
     int n;
     cout << "Enter number of elements" << endl;
@@ -26,6 +44,9 @@ int main(){
     for (int i = 0; i < n; i++){
         cin >> input[i];
     }
-    cout << firstIndex(input, n, 5) << endl;
-    
+    // cout << firstIndex(input, n, 5) << endl;
+    cout << "Enter elements for search" << endl;
+    int k;
+    cin >> k;
+    cout << lastIndex(input, n, k) << endl;
 }
