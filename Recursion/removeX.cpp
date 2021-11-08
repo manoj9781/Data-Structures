1dc876d232688096990f89090e884eb31db8ca1a
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-08 17:49:01 +0530

    Length of the string using recursion done

---
+++ Recursion/removeX.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+
+int length(char s[]){
+    if(s[0] == '\0'){
+        return 0;
+    }
+    int ans = length(s + 1);
+    return ans + 1;
+}
+
+int main(){
+    char str[100];
+    cin >> str;
+    cout << length(str);
+
+}
