f4ed53e19b0c03f7ed9d45fb6a696b77b5068cae
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-08 17:53:34 +0530

    Remove X character from a string done

--- Recursion/removeX.cpp
+++ Recursion/removeX.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
-
+void removeX(char s[]){
+    if(s[0] == '\0'){
+        return;
+    }
+    if(s[0] != 'x'){
+        removeX(s + 1);
+    }else{
+        int i = 1;
+        for (; s[i] != '\0';i++){
+            s[i - 1] = s[i];
+        }
+        s[i - 1] = '\0';
+        removeX(s);
+    }
+}
 
 int length(char s[]){
     if(s[0] == '\0'){
@@ -14,6 +28,9 @@ int length(char s[]){
 int main(){
     char str[100];
     cin >> str;
+    cout << length(str);
+    removeX(str);
+    cout <<'\n'<< str << endl;
     cout << length(str);
 
 }
