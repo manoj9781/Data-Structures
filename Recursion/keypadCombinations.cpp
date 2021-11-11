a05407dc28d8c40cd681cd343b65733d06ae1c30
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-11 19:07:28 +0530

    Return keypad Approach done

--- Recursion/keypadCombinations.cpp
+++ Recursion/keypadCombinations.cpp
@@ -1,21 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int keypad(int n, string output[], string dialer[]){
+int keypad(int num, string output[], string dialer[])
+{
+    if (num == 0)
+    {
+        output[0] = " ";
+        return 1;
+    }
+    int lastDigit = num % 10;
+    int smallInput = num / 10;
+    string smalloutput[100];
+    int smallAns = keypad(smallInput, smalloutput, dialer);
+    string op = dialer[lastDigit];
+    int k = 0;
+    for (int i = 0; i < op.size(); i++)
+    {
+        for (int j = 0; j < smallAns; j++)
+        {
+            output[k++] = smalloutput[j] + op[i];
+        }
+    }
 
+    return k;
 }
 
-int keypad(int n, string output[]){
+int keypad(int n, string output[])
+{
     string dialer[] = {" ", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     return keypad(n, output, dialer);
 }
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     string *output = new string[100];
     int count = keypad(n, output);
-    for (int i = 0; i < count; i++){
+    cout << count << endl;
+    for (int i = 0; i < count; i++)
+    {
         cout << output[i] << endl;
     }
 }
