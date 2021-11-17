fdb8d5424cf0e75e8ff71148f14ef4bcf2f0ddfe
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-17 15:51:56 +0530

    Deep copy done

--- OOPS/Student2.cpp
+++ OOPS/Student2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string.h>
 
 using namespace std;
 
@@ -9,7 +10,11 @@ class Student{
         public:
         Student(int age, char *name){
             this->age = age;
-            //Shallow Copy it uses the same address of the main block
-            this->name = name;
+            // //Shallow Copy it uses the same address of the main block
+            // this->name = name;
+
+           // Deep copy of the name array so it not changes if we pass name to another object.
+            this->name = new char[strlen(name) + 1];
+            strcpy(this->name, name);
         }
 };
