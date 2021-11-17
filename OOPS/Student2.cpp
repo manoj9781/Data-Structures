ffdfe7144150277e12c79f613c07b2b63fe150dc
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-17 16:44:55 +0530

    Use of initilization list for const variables

--- OOPS/Student2.cpp
+++ OOPS/Student2.cpp
@@ -1,34 +1,46 @@
-#include <iostream>
-#include <string.h>
+// #include <iostream>
+// #include <string.h>
 
-using namespace std;
+// using namespace std;
 
-class Student
-{
-private:
-    int age;
+// class Student
+// {
+// private:
+//     int age;
     
 
-public:
-char *name;
-    Student(int age, char *name)
-    {
-        this->age = age;
-        // //Shallow Copy it uses the same address of the main block
-        // this->name = name;
+// public:
+// char *name;
+//     Student(int age, char *name)
+//     {
+//         this->age = age;
+//         // //Shallow Copy it uses the same address of the main block
+//         // this->name = name;
 
-        // Deep copy of the name array so it not changes if we pass name to another object.
-        this->name = new char[strlen(name) + 1];
-        strcpy(this->name, name);
-    }
+//         // Deep copy of the name array so it not changes if we pass name to another object.
+//         this->name = new char[strlen(name) + 1];
+//         strcpy(this->name, name);
+//     }
 
-    //Copy Consructor
-    Student(Student const &s){
-        this->age = s.age;
-        this->name = new char[strlen(s.name) + 1];
-        strcpy(this->name, name);
-    }
-    void display(){
-        cout << this->name << " " << this->age << endl;
-    }
-};
+//     //Copy Consructor
+//     Student(Student const &s){
+//         this->age = s.age;
+//         this->name = new char[strlen(s.name) + 1];
+//         strcpy(this->name, name);
+//     }
+//     void display(){
+//         cout << this->name << " " << this->age << endl;
+//     }
+// };
+
+  //Initialization List 
+
+  class Studnet{
+      public:
+          int age;
+          int const rollNumber;
+
+          Studnet(int age, int rollNunber) : rollNumber(rollNumber), age(age){
+              cout << "Set constructor" << endl;
+          }
+  };
--- OOPS/Student2Use.cpp
+++ OOPS/Student2Use.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
 #include "Student2.cpp"
-int main(){
-    char name[] = "abcd";
-    Student s1(20, name);
+// int main(){
+//     char name[] = "abcd";
+//     Student s1(20, name);
 
-    name[3] = 'v';
-    Student s2(13, name);
-    s1.display();
-    s2.display();
-    s1.display();
+//     name[3] = 'v';
+//     Student s2(13, name);
+//     s1.display();
+//     s2.display();
+//     s1.display();
+// }
+
+int main(){
+    Studnet s1(12, 1001);
+    Studnet s2(14, 1002);
 }
