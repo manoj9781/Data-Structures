2f119a793bedc1568f5edc805d7faf618bdc2749
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-17 16:17:56 +0530

    Copy done contructor done

--- OOPS/Student2.cpp
+++ OOPS/Student2.cpp
@@ -1,20 +1,30 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
 
 using namespace std;
 
-class Student{
-    private:
-        int age;
-        char *name;
-        public:
-        Student(int age, char *name){
-            this->age = age;
-            // //Shallow Copy it uses the same address of the main block
-            // this->name = name;
+class Student
+{
+private:
+    int age;
+    char *name;
 
-           // Deep copy of the name array so it not changes if we pass name to another object.
-            this->name = new char[strlen(name) + 1];
-            strcpy(this->name, name);
-        }
+public:
+    Student(int age, char *name)
+    {
+        this->age = age;
+        // //Shallow Copy it uses the same address of the main block
+        // this->name = name;
+
+        // Deep copy of the name array so it not changes if we pass name to another object.
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+    }
+
+    //Copy Consructor
+    Student(Student const &s){
+        this->age = s.age;
+        this->name = new char[strlen(s.name) + 1];
+        strcpy(this->name, name);
+    }
 };
