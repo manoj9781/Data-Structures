7d2b7f4007de980afb41d2072de6ed34a2cc6a8f
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-18 12:50:26 +0530

    Operator overload done for + and *

--- OOPS/Fraction.cpp
+++ OOPS/Fraction.cpp
@@ -79,4 +79,23 @@ class Fraction{
         fNew.simplify();
         return fNew;
     }
+
+    Fraction operator+(Fraction const &f2) const{
+        int lcm = this->denominator * f2.denominator;
+        int x = lcm / this->denominator;
+        int y = lcm / f2.denominator;
+        int num = (x * this->numerator) + (y * f2.numerator);
+
+        Fraction fNew(num, lcm);
+        fNew.simplify();
+        return fNew;
+    }
+    Fraction operator*(Fraction const &f2) const{
+        int n = this->numerator * f2.numerator;
+        int d = this->denominator * f2.denominator;
+
+        Fraction fNew(n, d);
+        fNew.simplify();
+        return fNew;
+    }
 };
--- OOPS/FractionUse.cpp
+++ OOPS/FractionUse.cpp
@@ -5,12 +5,15 @@ int main(){
     Fraction f1(10, 2);
     Fraction f2(15, 4);
 
-    Fraction f3 = f1.Add(f2);
+    Fraction f3 = f1 + f2;
     f3.print();
-    f1.print();
-    f2.print();
-    Fraction f4 = f1.Multiply(f2);
-    f4.print();
+
+    // Fraction f3 = f1.Add(f2);
+    // f3.print();
+    // f1.print();
+    // f2.print();
+    // Fraction f4 = f1.Multiply(f2);
+    // f4.print();
 
     // f1.add(f2);
     // f1.print();
