5e0bac1025160792d2315fdf502cba81eeae991d
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-20 12:49:00 +0530

    Complete Pre decrement opeartor overload

--- OOPS/FractionUse.cpp
+++ OOPS/FractionUse.cpp
@@ -3,8 +3,7 @@ using namespace std;
 #include "Fraction.cpp"
 int main(){
     Fraction f1(10, 3);
-    Fraction f2(5, 2);
-    (f1 += f2) += f2;
     f1.print();
-    f2.print();
+    --(--f1);
+    f1.print();
 }
