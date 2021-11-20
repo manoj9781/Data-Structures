e5c636ee97e5f00bec1e1e3dee1c3e05f72f8ecc
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-20 12:42:37 +0530

    Test done for += opeartor

--- OOPS/FractionUse.cpp
+++ OOPS/FractionUse.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 #include "Fraction.cpp"
 int main(){
-    Fraction f1(10, 2);
+    Fraction f1(10, 3);
+    Fraction f2(5, 2);
+    (f1 += f2) += f2;
     f1.print();
-    f1++;
-    f1.print();
+    f2.print();
 }
