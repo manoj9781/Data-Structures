4c0137c725223f83486904e26cbd6c5bd82a9c20
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-23 10:52:19 +0530

    Markd some functions constant

--- OOPS/DynamicArray.cpp
+++ OOPS/DynamicArray.cpp
@@ -15,7 +15,7 @@ public:
         this->capacity = 5;
         this->nextIndex = 0;
     }
-    
+
     //Copy Constructor
     DynamicArray(DynamicArray const &d)
     {
@@ -27,20 +27,25 @@ public:
         this->capacity = d.capacity;
         this->nextIndex = d.nextIndex;
     }
-    
+
     //Copy Assignment operator
-    void operator=(DynamicArray const &d){
+    void operator=(DynamicArray const &d)
+    {
         this->data = new int[d.capacity];
-        for (int i = 0; i < nextIndex; i++){
+        for (int i = 0; i < d.nextIndex; i++)
+        {
             this->data[i] = d.data[i];
         }
         this->capacity = d.capacity;
         this->nextIndex = d.nextIndex;
     }
-    void add(int element){
-        if(nextIndex == capacity){
+    void add(int element)
+    {
+        if (nextIndex == capacity)
+        {
             int *newData = new int[capacity * 2];
-            for (int i = 0; i < nextIndex; i++){
+            for (int i = 0; i < nextIndex; i++)
+            {
                 newData[i] = data[i];
             }
             delete[] data;
@@ -50,27 +55,34 @@ public:
         data[nextIndex] = element;
         nextIndex++;
     }
-    int getElement(int index){
-        if(index < nextIndex){
+    int getElement(int index) const
+    {
+        if (index < nextIndex)
+        {
             return data[index];
         }
-        else{
+        else
+        {
             return -1;
         }
     }
-    void add(int index, int element){
-        if(index < nextIndex){
+    void add(int index, int element)
+    {
+        if (index < nextIndex)
+        {
             data[index] = element;
         }
-        else if(nextIndex == index){
+        else if (nextIndex == index)
+        {
             add(element);
         }
-        else{
+        else
+        {
             return;
         }
     }
 
-    void print()
+    void print() const
     {
         for (int i = 0; i < nextIndex; i++)
         {
