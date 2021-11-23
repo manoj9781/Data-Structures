b512391a9b585e4a979ebdcf4896e4a7de6b171c
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-23 10:18:04 +0530

    Copy constructor done

--- OOPS/DynamicArray.cpp
+++ OOPS/DynamicArray.cpp
@@ -1,15 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class DynamicArray{
-    private:
+class DynamicArray
+{
+private:
     int *data;
     int capacity;
     int nextIndex;
 
-    public:
-    void print(){
-        for (int i = 0; i < nextIndex; i++){
+public:
+    DynamicArray()
+    {
+        this->data = new int[5];
+        this->capacity = 5;
+        this->nextIndex = 0;
+    }
+    
+    //Copy Constructor
+    DynamicArray(DynamicArray const &d)
+    {
+        this->data = new int[d.capacity];
+        for (int i = 0; i < d.nextIndex; i++)
+        {
+            this->data[i] = d.data[i];
+        }
+        this->capacity = d.capacity;
+        this->nextIndex = d.nextIndex;
+    }
+
+    void print()
+    {
+        for (int i = 0; i < nextIndex; i++)
+        {
             cout << data[i] << " ";
         }
         cout << endl;
