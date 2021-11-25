6af1fe7d829dc464c486085234175d9904a0573f
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-25 18:57:31 +0530

    Number of 1 bit done

--- Patterns/numberof1bit.cpp
+++ Patterns/numberof1bit.cpp
@@ -37,3 +37,18 @@ Constraints:
 
 The input must be a binary string of length 32.
  
+
+ class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n!=0){
+            int bit = n&1;
+            if(bit == 1){
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+};
