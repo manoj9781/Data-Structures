62cdeac2ff162ae39395d061bc0b10efcfbe2ffa
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-25 18:53:54 +0530

    Power of 2 done

--- Patterns/powerof2.cpp
+++ Patterns/powerof2.cpp
@@ -41,4 +41,19 @@ Output: false
 Constraints:
 
 -231 <= n <= 231 - 1
- 
+ class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int ans = 1;
+        
+        for(int i = 0; i <= 30; i++){
+            if(ans == n){
+                return true;
+            }
+            if(ans < INT_MAX/2){
+                ans = ans *2;
+            }
+        }
+        return false;
+    }
+};
