68c7e08dbed439ceccc0cec5cf3f5f2b45044653
Manoj Singh <manojsinghrana467@gmail.com>
2021-11-26 17:34:06 +0530

    Palindrome done

--- Patterns/palindrome.cpp
+++ Patterns/palindrome.cpp
@@ -37,3 +37,26 @@ Output: false
 Constraints:
 
 -231 <= x <= 231 - 1
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int n = x;
+        int ans = 0;
+        while(n != 0){
+            int rem = n %10;
+            if((ans > INT_MAX/10) || (ans < INT_MIN/10)){
+              return false;
+            }
+             ans = (ans *10) + rem; 
+            n = n/10;
+        }
+        if(ans == x){
+            return true;
+        }
+        return false;
+    }
+};
