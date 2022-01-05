d0d8a4e4ecb76dd2bd2426ac2fe7c75c2198aca2
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-06 00:23:16 +0530

    Maximum Average Subarray Sum

---
+++ SlidingWindow/maximumAverage.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int maxSum = 0;
+        int size = nums.size();
+        for(int i = 0; i < k; i++){
+         maxSum += nums[i];   
+        }
+        int windowSum = maxSum;
+        for(int j = k; j < size; j++){
+            windowSum += nums[j] - nums[j-k];
+            maxSum = max(windowSum, maxSum);
+        }
+        cout << maxSum <<endl;
+        double ans = double(maxSum) / k;
+        return ans;
+    }
+};
