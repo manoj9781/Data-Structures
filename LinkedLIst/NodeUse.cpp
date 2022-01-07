43e4b4f204b0fcb117c5e25a1213739ec166cab1
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-08 00:00:25 +0530

    Merge Sort using Linked list Done

--- LinkedLIst/NodeUse.cpp
+++ LinkedLIst/NodeUse.cpp
@@ -324,10 +324,65 @@ Node *midPoint(Node *head){
     return slow;
 }
 
+ Node* mergeTwoLists(Node* head1, Node* head2) {
+        if(head1 == NULL){
+            return head2;
+        }
+        if(head2 == NULL){
+            return head1;
+        }
+        Node *head = NULL;
+        Node *tail = NULL;
+        
+        if(head1  -> data <= head2 -> data){
+            head = head1;
+            tail = head1;
+            head1 = head1 -> next;
+        }
+        else{
+            head = head2;
+            tail = head2;
+            head2 = head2 -> next;
+
+        }
+        
+        while(head1 != NULL && head2 != NULL){
+            if(head1 -> data <= head2 -> data){
+                tail -> next = head1;
+                tail = tail -> next;
+                head1 = head1 -> next;
+            }
+            else{
+                tail -> next = head2;
+                tail = tail -> next;
+                head2 = head2 -> next;
+            }
+        }
+        if(head1 == NULL){
+            tail -> next = head2;
+        }
+        if(head2 == NULL){
+            tail -> next = head1;
+        }
+        return head;
+    }
+
+Node *mergeSort(Node *head){
+    if(head == NULL || head -> next == NULL){
+        return head;
+    }
+    Node *mid = midPoint(head);
+    Node *head2 = mid -> next;
+    mid->next = NULL;
+    Node *ans = mergeTwoLists(mergeSort(head), mergeSort(head2));
+    return ans;
+}
+
 int main()
 {
     cout << "Enter value for linked list" << endl;
     Node *head = takeInputBetter();
-    Node *ans = midPoint(head);
-    cout << ans->data << endl;
+    Node *ans = mergeSort(head);
+    print(ans);
+    cout << endl;
 }
