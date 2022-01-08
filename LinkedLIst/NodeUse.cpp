e4860eaff0380abd36bae3d4f6bddb347cd07981
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-08 23:25:54 +0530

    Reverse Linked list using pair class

--- LinkedLIst/NodeUse.cpp
+++ LinkedLIst/NodeUse.cpp
@@ -378,11 +378,39 @@ Node *mergeSort(Node *head){
     return ans;
 }
 
+class Pair{
+    public:
+    Node *head;
+    Node *tail;
+};
+
+Pair reverse(Node *head){
+    if(head == NULL || head -> next == NULL){
+        Pair ans;
+        ans.head = head;
+        ans.tail = head;
+        return ans;
+    }
+
+    Pair smallAns = reverse(head->next);
+    smallAns.tail->next = head;
+    head->next = NULL;
+
+    Pair ans;
+    ans.head = smallAns.head;
+    ans.tail = head;
+    return ans;
+}
+
+Node *reverseBetter(Node *head){
+    return reverse(head).head;
+}
+
 int main()
 {
     cout << "Enter value for linked list" << endl;
     Node *head = takeInputBetter();
-    Node *ans = mergeSort(head);
+    Node *ans = reverseBetter(head);
     print(ans);
     cout << endl;
 }
