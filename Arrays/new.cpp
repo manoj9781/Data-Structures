40f45194403e10e8cbed8641352258b6dd8d0812
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-31 22:12:58 +0530

    Merge sort using linked list

--- Arrays/new.cpp
+++ Arrays/new.cpp
@@ -1,31 +1,15 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
 
-int maxScore(int *input, int n){
-    int sum = 0;
-    int minElement = INT_MAX;
-    for (int i = 0; i < n; i++){
-        sum += input[i];
-        if(input[i] < minElement){
-            minElement = input[i];
-        }
+int main()
+{
+    int n;
+    cin >> n;
+    int input[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
     }
-    int ans = minElement * n;
-    return ans + sum;
-}
-
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        int input[100];
-        for (int i = 0; i < n; i++){
-            cin >> input[i];
-        }
-        int ans = maxScore(input, n);
-        cout << ans << endl;
-    }
+    
 }
