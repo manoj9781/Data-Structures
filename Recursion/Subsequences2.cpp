f0bc393fc66321bcea82307b0b9420c9b7104d22
Manoj Singh <manojsinghrana467@gmail.com>
2022-01-31 22:46:18 +0530

    Print All Subsequences whose sum is equal to given sum

--- Recursion/Subsequences2.cpp
+++ Recursion/Subsequences2.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Print All Subsequences of the Array
 void printSubsequences(int index, vector<int> &ans, int input[], int n)
 {
     if (index == n)
@@ -25,6 +26,31 @@ void printSubsequences(int index, vector<int> &ans, int input[], int n)
     printSubsequences(index + 1, ans, input, n);
 }
 
+// Print All the Subsequences whose sum is equal to given sum
+
+void printS(int index, vector<int> &ans, int s, int *input, int n, int sum)
+{
+    if (index == n)
+    {
+        if (s == sum)
+        {
+            for (auto it : ans)
+            {
+                cout << it << " ";
+            }
+            cout << endl;
+        }
+
+        return;
+    }
+    ans.push_back(input[index]);
+    s += input[index];
+    printS(index + 1, ans, s, input, n, sum);
+    ans.pop_back();
+    s -= input[index];
+    printS(index + 1, ans, s, input, n, sum);
+}
+
 int main()
 {
     int n;
@@ -38,6 +64,8 @@ int main()
         cin >> input[i];
     }
     vector<int> ans;
-    cout << "SubSequences of the Array" << endl;
-    printSubsequences(0, ans, input, n);
+    int sum = 2;
+    printS(0, ans, 0, input, n, sum);
+    // cout << "SubSequences of the Array" << endl;
+    // printSubsequences(0, ans, input, n);
 }
