9b0e6779285ad49058483dd9662c5e5d4fa3c20a
Manoj Singh <manojsinghrana467@gmail.com>
2022-02-01 23:23:42 +0530

    Yet to come

---
+++ Recursion/subsetSum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Number of Elements " << endl;
+    cin >> n;
+    int *input = new int[n];
+
+    cout << "Enter Elements" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+    vector<int> ans;
+   
+}
