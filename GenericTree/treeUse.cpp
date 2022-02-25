8ab06a83cf426f52ff02c828ee8d49219478de50
Manoj Singh <manojsinghrana467@gmail.com>
2022-02-25 19:02:42 +0530

    Print Tree recursive

--- GenericTree/treeUse.cpp
+++ GenericTree/treeUse.cpp
@@ -1,22 +1,44 @@
-#include<iostream>
-#include"tree.h"
+#include <iostream>
+#include "tree.h"
 using namespace std;
 
-TreeNode<int> * takeInput(){
+TreeNode<int> *takeInput()
+{
     int data;
     cout << "Enter data" << endl;
     cin >> data;
-    TreeNode<int>* root = new TreeNode<int>(data);
+    TreeNode<int> *root = new TreeNode<int>(data);
     int n;
     cout << "Enter Number of child" << endl;
     cin >> n;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         TreeNode<int> *child = takeInput();
         root->children.push_back(child);
     }
     return root;
 }
 
-int main(){
+void printTree(TreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << ":";
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+    for (int i = 0; i < root->children.size(); i++)
+    {
+        printTree(root->children[i]);
+    }
+}
 
+int main()
+{
+    TreeNode<int> *root = takeInput();
+    printTree(root);
 }
