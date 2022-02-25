68dc0cd8fc9d9e3b8e4911727def12755dfc8a8d
Manoj Singh <manojsinghrana467@gmail.com>
2022-02-25 18:48:11 +0530

    Create Tree class

---
+++ GenericTree/tree.h
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+template<typename T>
+class TreeNode{
+    public:
+        T data;
+        vector<TreeNode *> children;
+        TreeNode(T data){
+            this->data = data;
+        }
+};
