9d2c289bdc0007cdcf2aebbeb438eff387e5995d
Manoj Singh <manojsinghrana467@gmail.com>
2022-03-17 15:50:01 +0530

    Inorder predecessor and Succesor

--- BinaryTree/BinaryTreeUse.cpp
+++ BinaryTree/BinaryTreeUse.cpp
@@ -294,6 +294,38 @@ BinaryTreeNode<int> *lowestCommonAcenstor(BinaryTreeNode<int> *root, BinaryTreeN
     }
 }
 
+pair<int,int> predecessorSuccessor(BinaryTreeNode<int>* root, int key)
+{
+    // Write your code here.
+    BinaryTreeNode<int> *temp = root;
+    int pre = -1;
+    int suc = -1;
+    while(temp -> data != key){
+        if(temp -> data > key){
+            suc = temp -> data;
+            temp = temp -> left;
+        }
+        else{
+            pre = temp -> data;
+            temp = temp -> right;
+        }
+    }
+    BinaryTreeNode<int> *leftTree = temp -> left;
+    while(leftTree != NULL){
+        pre = leftTree -> data;
+        leftTree = leftTree -> right;
+    }
+    BinaryTreeNode<int> *rightTree = temp -> right;
+    while(rightTree != NULL){
+        suc = rightTree -> data;
+        rightTree = rightTree -> left;
+    }
+    return {pre, suc};
+}
+
+
+
+
 int main()
 {
     BinaryTreeNode<int> *root = inputLevelWise();
