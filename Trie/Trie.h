b7f67146ee4d00d7d299aae8c2981705ed1b2bfd
Manoj Singh <manojsinghrana467@gmail.com>
2022-04-06 22:52:22 +0530

    Remove word function done

--- Trie/Trie.h
+++ Trie/Trie.h
@@ -28,8 +28,36 @@ class Trie{
         insertWord(child, word.substr(1));
     }
 
+    void removeWord(TrieNode *root, string word){
+        if(word.size() == 0){
+            root->isTerminal = false;
+            return;
+        }
+        TrieNode *child;
+        int index = word[0] - 'a';
+        if(root -> children[index] != NULL){
+            child = root->children[index];
+        }
+        else{
+            return;
+        }
+        removeWord(child, word.substr(1));
+        if(child -> isTerminal == false){
+            for (int i = 0; i < 26; i++){
+                if(child -> children[i] != NULL){
+                    return;
+                }
+            }
+            delete child;
+            root->children[index] = NULL;
+        }
+    }
+
     public:
     void insertWord(string word){
         insertWord(root, word);
+    }
+    void removeWord(string word){
+        removeWord(root, word);
     }
 };
