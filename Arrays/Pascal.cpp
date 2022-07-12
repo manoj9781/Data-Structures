cbe4d1eb46d3e32c871cc34f3ab4a58e472d6128
Manoj Singh <manojsinghrana467@gmail.com>
2022-07-12 14:07:16 +0530

    Edit Pascal Triangle

--- Arrays/Pascal.cpp
+++ Arrays/Pascal.cpp
@@ -5,18 +5,11 @@
         {
             for(int j = 0; j <= i; j++)
             {
-                // last and first column of triangle is "1"
                 if(j == 0 || j == i)
                     ans[i].push_back(1); 
                 
-                /*
-                here, we are using previous stored results in the 
-                vector, to fill next row. 
-                In the row 4, we can fill column 1, as 
-                {upper row's column 0 + column 1} and column 2, as
-                {upper row's column 1 + column 2} and so on....
-                Now, you can easily find the patttern.
-                */
+            
+              
                 else
                     ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
             }
