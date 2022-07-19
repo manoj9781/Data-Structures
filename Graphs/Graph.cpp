011b13cecfc58e07991ad44a55d0ea07c20f3b51
Manoj Singh <manojsinghrana467@gmail.com>
2022-07-19 13:05:14 +0530

    DFS Algorithm

--- Graphs/Graph.cpp
+++ Graphs/Graph.cpp
@@ -37,6 +37,34 @@ void bfs(int n, vector<int> adj[])
             bfs(n, i, adj, visited);
         }
     }
+    cout << endl;
+    delete[] visited;
+}
+
+/* DFS Algorithm */
+
+void dfs(int startVertex, vector<int> adj[], bool *visited){
+    cout << startVertex << " ";
+    visited[startVertex] = true;
+    for(auto it : adj[startVertex]){
+        if(!visited[it]){
+            dfs(it, adj, visited);
+        }
+    }
+}
+
+void dfs(int n, vector<int> adj[]){
+    bool *visited = new bool[n];
+    for (int i = 0; i < n; i++){
+        visited[i] = false;
+    }
+    for (int i = 0; i < n; i++){
+        if(!visited[i]){
+            dfs(i, adj, visited);
+        }
+    }
+    cout << endl;
+    delete[] visited;
 }
 
 int main()
@@ -52,6 +80,9 @@ int main()
         adj[firstVertex].push_back(secondVertex);
         adj[secondVertex].push_back(firstVertex);
     }
+    cout << "BFS" << endl;
     bfs(n, adj);
+    cout << "DFS" << endl;
+    dfs(n, adj);
     return 0;
 }
